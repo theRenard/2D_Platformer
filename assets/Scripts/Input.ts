9e4449c24c51ed94b34501db70f5d4345c02b452
Daniele Tabanella <daniele.tabanella@gmail.com>
2023-03-04 18:33:16 +0100

    feat(add gamepad input): add gamepad info

--- assets/Scripts/Input.ts
+++ assets/Scripts/Input.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, input, Input as _Input, EventKeyboard, KeyCode } from 'cc';
+import { _decorator, Component, input, Input as _Input, EventKeyboard, KeyCode, EventGamepad } from 'cc';
 const { ccclass, property } = _decorator;
 
 export enum Directions {
@@ -13,6 +13,7 @@ export enum Buttons {
 @ccclass('Input')
 export class Input extends Component {
 
+    @property gamepadDeadZone = 0.2;
     horizontalValue: number = 0;
     verticalValue: number = 0;
     jumpButton: boolean = false;
@@ -20,6 +21,8 @@ export class Input extends Component {
     start() {
         input.on(_Input.EventType.KEY_DOWN, this.onKeyDown, this);
         input.on(_Input.EventType.KEY_UP, this.onKeyUp, this);
+        input.on(_Input.EventType.GAMEPAD_INPUT, this.gamepadInput, this);
+
     }
 
     private onKeyDown(event: EventKeyboard) {
@@ -62,6 +65,20 @@ export class Input extends Component {
                 this.jumpButton = false;
         }
     }
+
+    private gamepadInput(e: EventGamepad) {
+        const gp = e.gamepad;
+        const { x, y } = gp.leftStick.getValue();
+        // const a = gp.buttonSouth.getValue();
+        // if (a === 1) this.isFiring();
+        // if (a === 0) this.isNotFiring();
+        if (x !== 0 || y !== 0) {
+          const xDir = x > this.gamepadDeadZone || x < -this.gamepadDeadZone ? x : 0;
+          const yDir = y > this.gamepadDeadZone || y < -this.gamepadDeadZone ? y : 0;
+            this.horizontalValue = xDir;
+            this.verticalValue = yDir;
+        }
+      }
 
     getButtonDown(button: Buttons) {
         switch (button) {
