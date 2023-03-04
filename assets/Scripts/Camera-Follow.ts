4ea8aa8a4a5683f8c533ddc4f917c231c2afe114
Daniele Tabanella <daniele.tabanella@gmail.com>
2023-03-04 10:36:59 +0100

    feat(input): input

--- assets/Scripts/Camera-Follow.ts
+++ assets/Scripts/Camera-Follow.ts
@@ -1,29 +1,24 @@
-import { _decorator, Component, Node, Vec3, v2, Vec2, v3 } from 'cc';
+import { _decorator, Component, Node, Vec3, v2, Vec2, v3, Camera, UITransform } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Camera_Follow')
 export class Camera_Follow extends Component {
-    @property(Node) target: Node = null;
+    @property({ type: Node }) target: Node = null;
+
+    @property follow = false;
 
     smoothSpeed: number = 0.05;
     maxLowerPoint = -158;
 
-    // update(deltaTime: number) {
-    //     const y = this.target.position.y;
-    //     const possibleY = y < this.maxLowerPoint ? this.maxLowerPoint : y;
-    //     const desiredPosition = v3(this.target.position.x * deltaTime * 10, possibleY, this.target.position.z);
-    //     const position = v3();
-    //     Vec3.lerp(position, this.node.position, desiredPosition, this.smoothSpeed);
-    //     this.node.position = position;
-
-    // }
-    lateUpdate(deltaTime: number) {
-        const position = this.target.getPosition();
-        const desiredPosition = v3(this.target.position.x, this.target.position.y, this.target.position.z);
-        // const position = v3();
-        // Vec3.lerp(position, this.node.position, desiredPosition, this.smoothSpeed);
-        this.node.setPosition(position);
-
+    update(deltaTime: number) {
+        if (!this.target) return;
+        if (!this.follow) return;
+        const y = this.target.position.y;
+        const possibleY = y < this.maxLowerPoint ? this.maxLowerPoint : y;
+        const desiredPosition = v3(this.target.position.x * deltaTime * 10, possibleY, this.target.position.z);
+        const position = v3();
+        Vec3.lerp(position, this.node.position, desiredPosition, this.smoothSpeed);
+        this.node.position = position;
     }
 }
 
--- assets/Scripts/PlayerController.ts
+++ assets/Scripts/PlayerController.ts
@@ -1,21 +1,30 @@
-import { _decorator, Component, Node, RigidBody2D, Vec2, Vec3 } from 'cc';
+import { _decorator, Component, Node, RigidBody2D, Vec2, Vec3, AnimationComponent, animation, Animation } from 'cc';
 import { Input, Directions, Buttons } from './Input';
 const { ccclass, property, executionOrder } = _decorator;
+import { Camera_Follow } from './Camera-Follow';
 
 @ccclass('PlayerController')
-@executionOrder(-1)
 export class PlayerController extends Component {
 
+    private cameraFollow: Camera_Follow = null;
     private rb: RigidBody2D = null;
     private movementInputDirection: number = 0;
     private input: Input = null;
+    private spriteNode: Node = null;
+    private isWalking: boolean = false;
     private isFacingRight: boolean = true;
     public movementSpeed: number = 10;
     public jumpForce: number = 16;
+    private animationController: animation.AnimationController = null;
 
     onLoad() {
+        this.spriteNode = this.node.getChildByName('Sprite');
         this.rb = this.getComponent(RigidBody2D);
         this.input = this.getComponent(Input);
+        this.cameraFollow = this.getComponent(Camera_Follow);
+        this.animationController = this.spriteNode.getComponent(animation.AnimationController);
+        console.log(this.spriteNode);
+        console.log(this.animationController);
     }
 
     private checkMovementDirection() {
@@ -24,11 +33,16 @@ export class PlayerController extends Component {
         } else if (this.movementInputDirection > 0 && !this.isFacingRight) {
             this.flip();
         }
+        if (this.rb.linearVelocity.x != 0) {
+            this.isWalking = true;
+        } else {
+            this.isWalking = false;
+        }
     }
 
     private flip() {
         this.isFacingRight = !this.isFacingRight;
-        // this.node.scale = new Vec3(-this.node.scale.x, 1);
+        this.node.scale = new Vec3(-this.node.scale.x, 1);
     }
 
     private jump() {
@@ -43,6 +57,10 @@ export class PlayerController extends Component {
         }
     }
 
+    private updateAnimations() {
+        this.animationController.setValue('isWalking', this.isWalking);
+    }
+
     private applyMovement() {
         this.rb.linearVelocity = new Vec2(this.movementInputDirection * this.movementSpeed, this.rb.linearVelocity.y);
     }
@@ -51,6 +69,12 @@ export class PlayerController extends Component {
         this.checkInput();
         this.applyMovement();
         this.checkMovementDirection();
+        this.updateAnimations();
+        // this.cameraFollow.updateCameraPosition(this.node.getPosition());
+    }
+
+    lateUpdate() {
+        // this.cameraFollow.updateCameraPosition(this.node.getPosition());
     }
 
 }
